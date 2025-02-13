c5bd975a944cecc368bc6708b5b9b5e39e4d8114
Nicolas Santucho <nicolaselias1998@gmail.com>
2025-02-13 12:19:11 -0300

    Escena Game: Feature marcar con imagen al hacer click en recuadro.

--- tic-tac-toe/src/scenes/GameScene.ts
+++ tic-tac-toe/src/scenes/GameScene.ts
@@ -21,11 +21,24 @@ export default class GameScene extends Phaser.Scene {
             for(let col=0;col<3;col++) {
                 const x=210*col+755;
                 const y=210*row+330;
+
                 const cell=this.add.rectangle(x,y,180,180,0xffffff).setStrokeStyle(2,0x000000).setInteractive();
+
+                cell.on('pointerdown',()=>this.handleCellClick(row,col,cell));
+
+                this.cells[row][col]=this.add.image(x,y,"").setVisible(false);
             }
         }
 
         var Fondo=this.add.image(960,540,'Fondo');
     }
 
+    handleCellClick(row:number,col:number,cell:Phaser.GameObjects.Rectangle){
+        if (this.board[row][col] !== "") return;
+        this.board[row][col]=this.currentPlayer;
+        this.clickSound.play();
+        const symbol = this.add.image(cell.x, cell.y, this.currentPlayer.toLowerCase());
+        symbol.setScale(0);
+        this.add.image(cell.x, cell.y,`${this.currentPlayer}`).setScale(0.5);
+    }
 };
