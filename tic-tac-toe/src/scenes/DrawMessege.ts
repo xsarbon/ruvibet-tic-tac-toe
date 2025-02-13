876ce12c562d18d7738bfea358af3e489e97f4c8
Nicolas Santucho <nicolaselias1998@gmail.com>
2025-02-13 15:14:59 -0300

    Funcionalidad Reiniciar en escena de empate.

--- tic-tac-toe/src/scenes/DrawMessege.ts
+++ tic-tac-toe/src/scenes/DrawMessege.ts
@@ -1,10 +1,27 @@
 import Phaser from "phaser";
 
 export default class DrawMessege extends Phaser.Scene {
+    private drawMessege!:Phaser.GameObjects.Text;
   constructor() {
     super("DrawMessege");
   }
+  preload() {
+    var messege:string="Empate! Pueden volver a jugar.";
+    this.drawMessege = this.add.text(960, 250, messege,
+      {
+        fontSize: '32px',
+        color: '#000',
+        fontFamily: 'Helvetica',
+      }
+    );
+    this.drawMessege.setOrigin(0.5, 0.5);
+  }
+
   create(){
-    this.add.image(960,560,"Reload").setScale(0.1);
+    var reload=this.add.image(960,560,"Reload").setScale(0.1);
+    reload.setInteractive();
+    reload.on('pointerdown', function (event) {
+      this.scene.start('GameScene');
+    }, this);
   }
 }
